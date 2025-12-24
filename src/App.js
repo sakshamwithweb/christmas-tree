1c3b6a3476745fd6f563d87d2227872db69bef3c
sakshamwithweb <websaksham39@gmail.com>
2025-12-24 22:44:22 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl bg-black w-screen h-screen text-white'>
+      My Name is Sak
+    </div>
+  )
+}
+
+export default App
